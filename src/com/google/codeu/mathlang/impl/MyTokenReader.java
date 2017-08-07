c9d7c624ea36ce28b94827303c5ec5d579231972
minimish <manishagupta@utexas.edu>
2017-08-07 01:49:01 -0500

    finished

--- src/com/google/codeu/mathlang/impl/MyTokenReader.java
+++ src/com/google/codeu/mathlang/impl/MyTokenReader.java
@@ -19,6 +19,15 @@ import java.io.IOException;
 import com.google.codeu.mathlang.core.tokens.Token;
 import com.google.codeu.mathlang.parsing.TokenReader;
 
+//imports to add
+import java.lang.NumberFormatException;
+import java.lang.StringBuilder;
+import com.google.codeu.mathlang.core.tokens.StringToken;
+import com.google.codeu.mathlang.core.tokens.NameToken;
+import com.google.codeu.mathlang.core.tokens.SymbolToken;
+import com.google.codeu.mathlang.core.tokens.NumberToken;
+
+
 // MY TOKEN READER
 //
 // This is YOUR implementation of the token reader interface. To know how
@@ -26,10 +35,16 @@ import com.google.codeu.mathlang.parsing.TokenReader;
 // You should not need to change any other files to get your token reader to
 // work with the test of the system.
 public final class MyTokenReader implements TokenReader {
+  private String source;
+  private int index;
+  private StringBuilder token;
 
   public MyTokenReader(String source) {
     // Your token reader will only be given a string for input. The string will
     // contain the whole source (0 or more lines).
+    this.source = source;
+    this.index = 0;
+    this.token = new StringBuilder();
   }
 
   @Override
@@ -41,6 +56,118 @@ public final class MyTokenReader implements TokenReader {
     // If for any reason you detect an error in the input, you may throw an IOException
     // which will stop all execution.
 
+    //read through all of the source
+    while(inBounds()) {
+      if(Character.isWhitespace(getChar())) {
+        keepGoing();
+      }
+      //handle quotes
+      else if(getChar() == '"') {
+        return inQuotes();
+      }
+      //handle all other cases
+      else {
+        return specialTokens(outOfQuotes());
+      }
+    } //end while statement
+    //end of input so return null
     return null;
+  }
+
+  public Token specialTokens(String tokenString) throws IOException {
+    if(tokenString.length() > 1) {
+      if(isNameToken(tokenString)) {
+        return new NameToken(tokenString);
+      }
+      else {
+        //try NumberToken, else must be a stringToken
+        try {
+          Double.parseDouble(tokenString);
+          return new NumberToken(Double.parseDouble(tokenString));
+        }
+        catch(NumberFormatException e) {
+          return new StringToken(tokenString);
+        }
+      }
+    } //end if statement
+    //edge case for when tokenString is only one character long
+    else {
+      char lastChar = tokenString.charAt(0);
+      //symbol
+      if (isSymbolToken(lastChar)) {
+        return new SymbolToken(lastChar);
+      }
+		  if(Character.isLetter(lastChar)) {
+		  	return new NameToken(tokenString);
+      } if (Character.isDigit(lastChar)){
+		  	return new NumberToken(Double.parseDouble(tokenString));
+		  }
+      //some other character
+      else {
+		  	throw new IOException("Error: Input character not defined");
+		  }
+		} //end edge case
+  }
+
+  private String outOfQuotes() throws IOException {
+    token.setLength(0);
+    //check inbounds and a token that is not whitespace
+    while(inBounds() && !Character.isWhitespace(getChar())) {
+      if(token.length() == 0) {
+        token.append(keepGoing());
+      }
+      else {
+    		int previousIndex = token.length() - 1;
+        char lastToken = token.charAt(previousIndex);
+    		if(lastToken == '=' || lastToken == '+' || isSymbolToken(getChar()))
+	    		return token.toString();
+        else
+	    		token.append(keepGoing());
+    	} //end else statement
+    } //end of while
+    return token.toString();
+  }
+
+  //read tokens in quotes
+  private Token inQuotes() throws IOException {
+    token.setLength(0);
+    if(keepGoing() != '"') {
+      throw new IOException("String needs to start with quotes");
+    }
+    //keep going until end quotes
+    while(getChar() != '"') {
+      token.append(keepGoing());
+    }
+    keepGoing();
+    return new StringToken(token.toString());
+  }
+
+  //increments the index and gets the character
+  private char keepGoing() throws IOException {
+    final char c = getChar();
+    index++;
+    return c;
+  }
+
+  //returns the character at index
+  private char getChar() throws IOException {
+    if(index >= source.length()) {
+      throw new IOException("Error: Index Out of Bounds.");
+    } else
+    return source.charAt(index);
+  }
+
+  //next three methods are helper checks
+
+  private boolean inBounds() {
+    return (source.length() - index) > 0;
+  }
+
+  private boolean isNameToken(String tokenString) {
+    return tokenString.equals("note") || tokenString.equals("print") || tokenString.equals("let");
+  }
+
+  private boolean isSymbolToken(char c) {
+    return c == '+' || c == '-' || c == '=' || c == ';';
   }
 }
